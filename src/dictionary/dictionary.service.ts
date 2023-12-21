db06d0b7c59bd4746df3f01803e764c7b693adf6
Glokamole <thomasguigard@gmail.com>
2023-12-21 13:29:45 -0500

    add general comments about current code

--- src/dictionary/dictionary.service.ts
+++ src/dictionary/dictionary.service.ts
@@ -1,3 +1,4 @@
+// TODO: You can move dictionary and words in the same folder as it belong to the same domain
 import { Word } from "../word/word";
 import { IWordProvider } from "../word/word.providers";
 import { Dictionary } from "./dictionary";
@@ -8,16 +9,19 @@ export class DictionaryService {
   private _dictionaryPresenter!: IDictionaryPresenter;
   private _dictionary: Dictionary = new Dictionary();
 
+  // TODO: add method return type
   setDictionaryPresenter(presenter: IDictionaryPresenter) {
     this._dictionaryPresenter = presenter;
     return this;
   }
 
+  // TODO: add method return type
   setWordProvider(provider: IWordProvider) {
     this._wordProvider = provider;
     return this;
   }
 
+  // TODO: add method return type
   async lookup(words: string[]) {
     let wordPromises: Promise<Word>[] = [];
     let errors: string[] = [];
@@ -25,6 +29,8 @@ export class DictionaryService {
     words.forEach((word) =>
       wordPromises.push(this._wordProvider.fetch({ name: word }))
     );
+
+    // TODO: How do you handle errors here ? Maybe a unit test can help you ;)
     const res = await Promise.allSettled(wordPromises);
 
     res.forEach((response) => {
@@ -36,6 +42,8 @@ export class DictionaryService {
       this._dictionary.addWord(response.value);
     });
 
+    // TODO: Make a public function to display dictionnary from your main file, eg: index.ts
+    //    Because of SRP your function should only lookup for words but not display the dictionary content
     this._dictionaryPresenter.displayDictionary(this._dictionary);
     console.log("Errors: \n", errors);
   }
--- src/word/word.providers.ts
+++ src/word/word.providers.ts
@@ -4,6 +4,7 @@ type WordToFetch = {
   name: string;
 };
 
+// TODO: I would move this error to it's proper file as you may need to intercept it in your dictionary.service
 export class WordNotFoundError extends Error {
   constructor(word: string) {
     super(word);
@@ -18,12 +19,16 @@ export interface IWordProvider {
 export class InMemoryWordProvider implements IWordProvider {
   private _words: Set<string> = new Set(["dog", "cat"]);
   private _wordArray = Array.from(this._words);
-
   fetch(word: WordToFetch): Promise<Word> {
+    // TODO: you can use Array.find() here
     const wordIndex = this._wordArray.indexOf(word.name);
     return new Promise((resolve, reject) =>
       setTimeout(() => {
+        // TODO: so you don't have to check index here
         if (wordIndex === -1) reject(new WordNotFoundError(word.name));
+        // TODO: and you won't have to get object from array here
+        //    also what happens if the word is an object like a spoon ?
+        //    and what happens if it's not a noun ?
         resolve(new Word(this._wordArray[wordIndex], "noun", "An animal"));
       }, 300)
     );
